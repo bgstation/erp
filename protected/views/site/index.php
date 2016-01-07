5cc97d87c1436b3a67f8b3f2f04c278c28fd5d8c
Pedro Jordão <pedro@didaticotecnologia.com.br>
2016-01-07 13:17:33 -0200

    Ocultação do botão de clientes no site/index

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,16 +6,16 @@ $this->pageTitle = Yii::app()->name;
 
 <div class="span12">
     <?php
-    $this->widget('bootstrap.widgets.TbButton', array(
-        'type' => 'primary',
-        'size' => 'large',
-        'label' => 'Novo cliente',
-        'url' => Yii::app()->createUrl('cliente/create'),
-        'htmlOptions' => array(
-            'class' => 'pull-left',
-        ),
-            )
-    );
+//    $this->widget('bootstrap.widgets.TbButton', array(
+//        'type' => 'primary',
+//        'size' => 'large',
+//        'label' => 'Novo cliente',
+//        'url' => Yii::app()->createUrl('cliente/create'),
+//        'htmlOptions' => array(
+//            'class' => 'pull-left',
+//        ),
+//            )
+//    );
     ?>
 </div>
 
